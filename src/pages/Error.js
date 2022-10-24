92c1f7b4f9fa34ceb14e7c23cce8bf4a131fab0d
Clément Théraud <theraud.clement@gmail.com>
2022-10-24 12:43:49 +0200

    create error.js
    js for error page

---
+++ src/pages/Error.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const Error = () => {
+  return (
+    <div style={{padding: '15%', fontSize: '6rem', fontFamily: 'Source Sans Pro', fontWeight: 'bold', color: 'var(--green-dark)'}}>This page doesn't exist..!
+    </div>
+  )
+}
+
+export default Error
