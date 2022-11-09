894f5c7e94eaa9fb437c60884c0b03f4b06c6c26
Clément Théraud <theraud.clement@gmail.com>
2022-11-09 15:05:30 +0100

    upload plugin maj

--- src/pages/CreateEmployee.js
+++ src/pages/CreateEmployee.js
@@ -12,6 +12,8 @@ import { useEmployeesContext } from '../context/employeesContext'
  * @returns a form with a few inputs and a submit button.
  */
 console.log(DropdownMenu);
+console.log('plugin-dropdown-menu-select');
+
 const Create = () => {
   //options dpt
   const departmentOptions = ["Sales", "Marketing", "Engineering", "Human Resources", "Legal"]
